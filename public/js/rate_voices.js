aa2555280fa70edacc20211721e304390fe40d9f
Nina Cecilie Højholdt <nhoejholdt@gmail.com>
2018-02-16 21:38:59 +0100

    Add routes and view files for the last pages

---
+++ public/js/rate_voices.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function() {
+
+    /*
+    sendToPath('get', 'api/recordings/next', {}, function (error, response) {
+        if(error) {
+            console.log(error);
+        } else {
+            console.log(response);
+        }
+    });
+    */
+
+});
--- routes/frontend/pages.js
+++ routes/frontend/pages.js
@@ -9,8 +9,16 @@ router.get('/about', function(req, res, next) {
     res.render('pages/about');
 });
 
+router.get('/contact', function(req, res, next) {
+    res.render('pages/contact');
+});
+
 router.get('/contribute-voice', function(req, res, next) {
     res.render('pages/contribute-voice');
+});
+
+router.get('/rate-voices', function(req, res, next) {
+    res.render('pages/rate-voices');
 });
 
 module.exports = router;
